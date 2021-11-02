6609085a4ca464ed99d870fa2627b1fe04f950b7
Nyanm <50958314+Nyanm@users.noreply.github.com>
2021-11-02 16:49:53 +0800

    F**k SHIFT-JIS

--- main.py
+++ main.py
@@ -74,13 +74,13 @@ class Convertor:
                     .replace("齷", "é").replace("騫", "á").replace("曦", "à").replace("龕", "€").replace("趁", "Ǣ") \
                     .replace("蹇", "₂").replace("彜", "ū").replace("雋", "Ǜ").replace("隍", "Ü").replace("鬻", "♃") \
                     .replace("鬥", "Ã").replace("鬆", "Ý").replace("齶", "♡").replace("齲", "❤").replace("躔", "★") \
-                    .replace('釁', '🍄')
+                    .replace('釁', '🍄').replace('頽', 'ä')
                 artist = root[index][0][3].text \
                     .replace("驫", "ā").replace("骭", "ü").replace("驩", "Ø").replace("罇", "ê").replace("曩", "è") \
                     .replace("齷", "é").replace("騫", "á").replace("曦", "à").replace("龕", "€").replace("趁", "Ǣ") \
                     .replace("蹇", "₂").replace("彜", "ū").replace("雋", "Ǜ").replace("隍", "Ü").replace("鬻", "♃") \
                     .replace("鬥", "Ã").replace("鬆", "Ý").replace("齶", "♡").replace("齲", "❤").replace("躔", "★") \
-                    .replace('釁', '🍄')
+                    .replace('釁', '🍄').replace('頽', 'ä')
                 bpm_max = int(root[index][0][6].text) / 100
                 date = root[index][0][8].text
                 version = int(root[index][0][13].text)
