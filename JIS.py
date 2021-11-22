547f4e462b1330e1ee667ed90dc53e0a740465d5
Nyanm <50958314+Nyanm@users.noreply.github.com>
2021-11-22 11:07:24 +0800

    Separate SHIFT-JIS issues from main body

---
+++ JIS.py
@@ -0,0 +1,8 @@
+# Special page for this shitty code format
+def jis_cvt(name: str) -> str:
+    return \
+        name.replace("驫", "ā").replace("骭", "ü").replace("驩", "Ø").replace("罇", "ê").replace("曩", "è") \
+            .replace("齷", "é").replace("騫", "á").replace("曦", "à").replace("龕", "€").replace("趁", "Ǣ") \
+            .replace("蹇", "₂").replace("彜", "ū").replace("雋", "Ǜ").replace("隍", "Ü").replace("鬻", "♃") \
+            .replace("鬥", "Ã").replace("鬆", "Ý").replace("齶", "♡").replace("齲", "❤").replace("躔", "★") \
+            .replace('釁', '🍄').replace('頽', 'ä').replace('黻', '*')
