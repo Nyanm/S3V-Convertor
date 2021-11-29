0e70b66c19430a74067ec9da82ad716f4ca3b4d6
Nyanm <50958314+Nyanm@users.noreply.github.com>
2021-11-29 10:20:32 +0800

    Update JIS.py

--- JIS.py
+++ JIS.py
@@ -1,8 +1,18 @@
 # Special page for this shitty code format
-def jis_cvt(name: str) -> str:
-    return \
-        name.replace("驫", "ā").replace("骭", "ü").replace("驩", "Ø").replace("罇", "ê").replace("曩", "è") \
-            .replace("齷", "é").replace("騫", "á").replace("曦", "à").replace("龕", "€").replace("趁", "Ǣ") \
-            .replace("蹇", "₂").replace("彜", "ū").replace("雋", "Ǜ").replace("隍", "Ü").replace("鬻", "♃") \
-            .replace("鬥", "Ã").replace("鬆", "Ý").replace("齶", "♡").replace("齲", "❤").replace("躔", "★") \
-            .replace('釁', '🍄').replace('頽', 'ä').replace('黻', '*')
+def amend_jis(jis_str: str) -> str:
+    if not jis_str:
+        return ''
+    new = jis_str
+    # latin field
+    new = new\
+        .replace("驫", "ā").replace("騫", "á").replace("曦", "à").replace('頽', 'ä').replace("罇", "ê").replace("曩", "è")\
+        .replace("齷", "é").replace("彜", "ū").replace("鬥", "Ã").replace("雋", "Ǜ").replace("隍", "Ü").replace("趁", "Ǣ")\
+        .replace("鬆", "Ý").replace("驩", "Ø")
+    # symbol field
+    new = new\
+        .replace("龕", "€").replace("蹇", "₂").replace("鬻", "♃").replace('黻', '*')
+    # graph field
+    new = new\
+        .replace("齶", "♡").replace("齲", "❤").replace("躔", "★").replace('釁', '🍄').replace('齪', '♣').replace('鑈', '♦')\
+        .replace('霻', '♠')
+    return new
